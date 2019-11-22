b97330e47628a7ec96771145d0b03595ced00226
Alejandro M. BERNARDIS <alejandro.bernardis@gmail.com>
2019-11-22 15:24:29 -0300

    UPD // +Features

---
+++ aysa/docker/registry/cli.py
@@ -0,0 +1,24 @@
+# Author: Alejandro M. BERNARDIS
+# Email: alejandro.bernardis@gmail.com
+# Created: 2019/11/22 15:17
+# ~
+"""
+AySA / Docker Registry Api Client (cli)
+
+usage:
+    aysar [options] COMMAND [ARGS...]
+
+Opcionales:
+    -y, --yes
+
+"""
+
+import sys
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    sys.exit(main())
